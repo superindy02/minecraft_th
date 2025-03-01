77c570a70df6162e3445261761e5e239633f4b6a
Minecraft-TH <56667939+superindy02@users.noreply.github.com>
2025-03-01 07:22:27 +0700

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css';
-import useAuthStore from '../hooks/useAuth';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
